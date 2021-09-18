610b68cf8e01b3e8fccfbf820a45928c09e4d149
Akshay Karande <karande.akshay777@gmail.com>
2021-09-18 17:50:48 +0530

    Remove header logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { MenuOpen, GitHub, RemoveRedEye } from '@material-ui/icons';
+import { MenuOpen, GitHub } from '@material-ui/icons';
 import './header.scss';
 
 function Header(props) {
     return (
         <div className="header d-flex">
             <div className="d-flex align-items-center">
-                <RemoveRedEye className="d-none d-md-block logo" />
                 <MenuOpen
                     onClick={() => props.toggleMenu()}
                     className="d-md-none d-sm-block menuIcon"
                     color="primary"
                 />
-                <h5 className="heading">see algorithms</h5>
+                <h5 className="heading">SEE ALGORITHMS</h5>
             </div>
             <div>
                 <GitHub
